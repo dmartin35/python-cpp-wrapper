3c5101db17d94d71847cbe3e07e4234167d0da54
David Martin <dmartin35@users.noreply.github.com>
2017-05-12 00:19:58 +0200

    updates tests for py.test

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 import demo
 
-msg = 'This is a simple message!'
-echoed = demo.echo(msg)
-assert type(echoed) == str
-assert echoed == msg
 
-today = demo.now()
-assert type(today) == dict
+def test_echo():
+    msg = 'This is a simple message!'
+    echoed = demo.echo(msg)
+    assert type(echoed) == str
+    assert echoed == msg
 
-sum = demo.addition(5, 6)
-assert type(sum) == int
-assert sum == 11
+
+def test_now():
+    today = demo.now()
+    assert type(today) == dict
+
+
+def test_addition():
+    sum = demo.addition(5, 6)
+    assert type(sum) == int
+    assert sum == 11
